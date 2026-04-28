400eb980901c5a6654271a8e73d645163a03e236
Jay Verstraete <104129830+jayf0x@users.noreply.github.com>
2026-04-28 18:26:08 +0200

    Update sync-site-3d.sh

--- scripts/sync-site-3d.sh
+++ scripts/sync-site-3d.sh
@@ -3,15 +3,15 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 REPO_ROOT="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 REAL_SITE="$REPO_ROOT/site/.inspiration/htmlto-canvas"
-PUBLC_SITE="$REPO_ROOT/site-3d"
+PUBLIC_SITE="$REPO_ROOT/site-3d"
 
 cd $REAL_SITE
 
 
-rsync -av --exclude-from=.gitignore $REAL_SITE/ $PUBLC_SITE
+rsync -av --exclude-from=.gitignore $REAL_SITE/ $PUBLIC_SITE
 
 
-# cp -R src stages *.md "$PUBLC_SITE"
+# cp -R src stages *.md "$PUBLIC_SITE"
 
 
 
